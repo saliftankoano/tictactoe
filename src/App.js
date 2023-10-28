77f8fff230787f6d131be70a3680e90401878e53
Salif Tankoano <tanksalif@gmail.com>
2023-10-28 15:52:46 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Tic-React-Toe</h1>
+        <h2>It's X's turn!</h2>
+        <div class="flex-container flex-column">
+          <div class="flex-container flex-wrap" id="board">
+              <div class="square"></div>
+              <div class="square"></div>
+              <div class="square"></div>
+              <div class="square"></div>
+              <div class="square"></div>
+              <div class="square"></div>
+              <div class="square"></div>
+              <div class="square"></div>
+              <div class="square"></div>
+          </div>
+          <button id="reset-button">reset</button>
+        </div>
+      </div>
     </div>
   );
 }
