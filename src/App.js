726688ee7479bc48bd279459d6aed5d1a3792b8e
Salif Tankoano <tanksalif@gmail.com>
2023-10-28 16:07:10 -0400

    return to orginal state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <h1>Tic-React-Toe</h1>
-        <h2>It's X's turn!</h2>
-        <div class="flex-container flex-column">
-          <div class="flex-container flex-wrap" id="board">
-              <div class="square"></div>
-              <div class="square"></div>
-              <div class="square"></div>
-              <div class="square"></div>
-              <div class="square"></div>
-              <div class="square"></div>
-              <div class="square"></div>
-              <div class="square"></div>
-              <div class="square"></div>
-          </div>
-          <button id="reset-button">reset</button>
-        </div>
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
